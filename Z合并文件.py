6c7331fb44427bf69834a9e304ba885a487407af
通晓宇宙 <2581011320@qq.com>
2022-05-11 22:55:00 +0800

    开始写总结

---
+++ Z合并文件.py
@@ -0,0 +1,16 @@
+class MergeFiles:
+    def __init__(self, *args):
+        self.files = args
+
+    def merge_files(self, outfilename, encoding='utf-8', sep='\n'):
+        with open(outfilename, 'w', encoding=encoding) as fout:
+            for filename in self.files:
+                with open(filename, 'r', encoding=encoding) as fin:
+                    fout.write(fin.read())
+                    fout.write(sep)
+
+
+if __name__ == '__main__':
+    merge = MergeFiles('Summary-HDFPATP.md',
+                       'Vocabulary analysis-HDFPATP.md')
+    merge.merge_files('Z预览-全选-复制到word调整字体颜色-字号.md')
